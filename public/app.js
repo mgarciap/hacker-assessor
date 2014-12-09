00610e35b8f93ad1a57f22d4dcbf0c5a9a4ed2a6
Dylson Valente Neto <dylsonvalenteneto@gmail.com>
2014-12-09 19:13:06 -0300

    Added basic functionallity

--- public/app.js
+++ public/app.js
@@ -1,9 +1,18 @@
 (function(){ 
-  var app = angular.module('catalogo',['ui.bootstrap']);
+  var app = angular.module('catalogo',['ui.bootstrap']),
+      body = document.body,
+      nivel = document.getElementById('nivel'),
+      experiencia = document.getElementById('experiencia'),
+      comentario = document.getElementById('comentario'),
+      height = angular.element(body).css('padding-bottom');
 
   app.controller('CatalogoCtrl', ['$scope','$http', function ($scope,$http) {
+    $scope.answers = [];
     $scope.categories = [];
-    $scope.skills =[]
+    $scope.skills = [];
+    $scope.isVisible = false;
+    $scope.skillName;
+    $scope.skillID;
 
     $http.get('api/1/categories.json').success(function(data){
       $scope.categories = data;
@@ -13,16 +22,32 @@
       $scope.skills = data;
     });
 
-    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.loadForm = function(skillID,skillName) {
+      angular.element(body).css('padding-bottom', height);
+      $scope.skillName = skillName;
+      $scope.skillID = skillID;
+      $scope.isVisible = true;
+    }
 
-    $scope.addItem = function() {
-      var newItemNo = $scope.items.length + 1;
-      $scope.items.push('Item ' + newItemNo);
-    };
+    $scope.closeForm = function() {
+      angular.element(nivel).val(1);
+      angular.element(experiencia).val(null);
+      angular.element(comentario).val(null);
+      $scope.skillName = null;
+      $scope.skillID = null;
+      angular.element(body).css('padding-bottom', '0px');
+      $scope.isVisible = false;
+    }
 
-    $scope.status = {
-      isFirstOpen: false,
-      isFirstDisabled: false
-    };
+    $scope.saveExperience = function() {
+      var answer = {
+        nivel: angular.element(nivel).val(),
+        experiencia: angular.element(experiencia).val(),
+        comentario: angular.element(comentario).val(),
+        skill_id: $scope.skillID
+      }
+      $scope.answers.push(answer);
+      console.log(JSON.stringify($scope.answers));
+    }
   }]);
 })();
