3c10fd971c1aedc5db46133203b7bce626da65c0
Eloy Espinaco <eloyesp@gmail.com>
2015-08-27 16:22:56 -0300

    Add basic dynamic stuff

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var ecstatic = require("ecstatic");
 var http = require('http');
+var ejs = require('ejs');
+var fs = require('fs');
+
+
+var staticsFiles = ecstatic({ root: __dirname + '/public' });
  
-http.createServer(
-  ecstatic({ root: __dirname + '/public' })
-).listen(8080);
+http.createServer(function(req, res) {
+  if (req.url === '/skills/tdd.html') {
+    skill('tdd', res);
+  } else {
+    staticsFiles(req, res);
+  }
+}).listen(8080);
  
-console.log('Listening on :8080');
+console.log('Listening on http://localhost:8080');
+
+function skill(skill_name, res) {
+  var skill = require( __dirname + '/app/skills/' + skill_name + '.json');
+  fs.readFile(__dirname + '/templates/skills.html.ejs', { encoding: 'utf8' },
+      function(err, string) {
+        console.log(string);
+        var template = ejs.compile(string);
+        res.end(template(skill));
+  });
+}
