35fb1e1b1a5ecc5390c46048e8fec903f5b96274
Eloy Espinaco <eloyesp@gmail.com>
2015-08-28 14:42:21 -0300

    Enumerate lacking skills.

--- server.js
+++ server.js
@@ -3,17 +3,26 @@ var http = require('http');
 var ejs = require('ejs');
 var fs = require('fs');
 
+var port = process.env["PORT"] || 8080;
+
+// hardcoded stuff
+var hackers = require('./hackers.json');
+var levels = require('./levels.json');
+
 var staticsFiles = ecstatic({ root: __dirname + '/public' });
 
 http.createServer(function(req, res) {
-  if (req.url === '/skills/tdd.html') {
-    skill('tdd', res);
+  if (req.url === '/become/senior-frontend.html') {
+    show_instructions(req, res, {
+      hacker: hackers['Jorge'],
+      level: levels['Senior Frontend']
+    });
   } else {
     staticsFiles(req, res);
   }
-}).listen(8080);
+}).listen(port);
 
-console.log('Listening on http://localhost:8080');
+console.log('Listening on http://localhost:' + port );
 
 function skill(skill_name, res) {
   var skill = require( __dirname + '/app/skills/' + skill_name + '.json');
@@ -26,4 +35,16 @@ function skill(skill_name, res) {
         res.setHeader("Content-Type", "text/html");
         res.end(template(skill));
   });
+}
+
+function show_instructions(req, res, params) {
+  var lackingSkills = params.level.skills.filter(function(i) {
+    return params.hacker.skills.indexOf(i) < 0;
+  });
+
+  res.end(lackingSkills.join('\n'));
+}
+
+function template (file, cb) {
+  fs.readFile(__dirname + '/templates/', { encoding: 'utf8' }, cb);
 }
