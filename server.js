3e5d313e84210ee64a315a1b1d852d083d8014b7
lcostantini <costantinileandro1@gmail.com>
2015-09-07 15:53:33 -0300

    Add level description for the skills
    Make a custom level description comparing the required level for a
    skill and the current level for a hacker in that skill.

--- server.js
+++ server.js
@@ -58,13 +58,15 @@ function show_instructions(req, res, params) {
     for (var i=0; i < skills.length; i++) {
       if (skills[i].name === skill.name) {
         skills[i].level = SKILL_LEVELS[skill.level];
+        skills[i].pathDescription = buildLevelDescription(skills[i]);
         return skills[i];
       }
     }
     return {
       name: skill.name,
       level: SKILL_LEVELS[skill.level],
-      description: "This skill isn't registered yet in our database."
+      description: "This skill isn't registered yet in our database.",
+      pathDescription: buildLevelDescription(skill)
     };
   }
 
@@ -76,6 +78,31 @@ function show_instructions(req, res, params) {
   });
 }
 
+function buildLevelDescription(skill) {
+  level = hackerSkillLevel(skill.name)
+  if (level > -1) {
+    if ((SKILL_LEVELS.indexOf(skill.level) - level) === 1) {
+      return "Almost! You are so close to have this skill. " + skill.levelDescription +
+        " Your actual level is " + SKILL_LEVELS[level] + " and you need " + skill.level + "."
+    } else {
+      return skill.levelDescription +
+        " You need to improve this skill because you actual level is " +
+        SKILL_LEVELS[level] + " and you need " + skill.level + "."
+    }
+  }
+  return "You don't have any knowledge on this skill. " + skill.levelDescription +
+    " You can start reading the lectures below."
+};
+
+function hackerSkillLevel(name) {
+  var skills = hackers['Jorge'].skills;
+  for (var i=0; i < skills.length; i++) {
+    if (skills[i].name === name) {
+      return skills[i].level;
+    }
+  }
+  return -1;
+};
 
 /**
  * Make a template file available to work with.
@@ -90,7 +117,6 @@ function show_instructions(req, res, params) {
 function template (fileName, callback) {
   fs.readFile(__dirname + '/templates/' + fileName, { encoding: 'utf8' }, callback);
 }
-
 
 /**
  * Retrieve a named seniority from the database.
