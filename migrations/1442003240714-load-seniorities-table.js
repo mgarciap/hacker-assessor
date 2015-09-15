591c1ebec1825f3beef1398b2bb71f8f9e6dc01e
Dylson Valente Neto <dylsonvalenteneto@gmail.com>
2015-09-14 21:23:09 -0300

    Update migration that load seniorities in the db

--- migrations/1442003240714-load-seniorities-table.js
+++ migrations/1442003240714-load-seniorities-table.js
@@ -4,17 +4,9 @@ var data = require('../data/json/seniorities.json');
 var query;
 
 for (item in data) {
-  item = data[item];
-
-  var list = [];
-  item.requirements.forEach(function(requirement) {
-    var piece = JSON.stringify(requirement).split(",");
-    list = list.concat(piece);
-  });
-
   query = seniorities.insert({
-    name: item.name,
-    requirements: list.join(",")
+    name: data[item].name,
+    requirements: JSON.stringify(data[item].requirements)
   }).toQuery();
 
   connection().query(query.text, query.values, connection.logger);
