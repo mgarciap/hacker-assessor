d9596e306c2b8b18a8f538d2e62c01b41d4fd1bd
lcostantini <costantinileandro1@gmail.com>
2015-09-16 11:52:09 -0300

    Fix test to work with the new refactor

--- test/hacker_test.js
+++ test/hacker_test.js
@@ -1,50 +1,74 @@
 var tap = require('tap');
-var app = require('../app');
 var utils = require('../utils');
-var skills = require('../data/json/skills.json');
+
+var hacker = { id: 1,
+               name: 'Jorge',
+               skills:
+                 [ { name: 'TDD', level: 1 },
+                   { name: 'JavaScript', level: 3 },
+                   { name: 'DevTools', level: 3 },
+                   { name: 'Angular', level: 1 } ] };
 
 var seniority = { id: 1,
-  name: 'Senior Frontend',
-  requirements: '{"name":"Discuss Dependencies","level":2},{"name":"Time Traveler","level":3}' };
+                  name: 'Senior Frontend',
+                  requirements:
+                    [ { name: 'Discuss Dependencies', level: 2 },
+                      { name: 'TDD', level: 2 },
+                      { name: 'JavaScript', level: 3 },
+                      { name: 'Time Traveler', level: 3 },
+                      { name: 'Angular', level: 3 },
+                      { name: 'VanillaJS', level: 2 },
+                      { name: 'Scrum Master', level: 0 } ] };
 
-var parse = utils.parseSeniority(seniority);
+tap.test('Get all the needed skills for a single hacker', function(t) {
+  var neededSkills = utils.hackerNeededSkills(hacker, seniority);
+  t.equal('Jorge', neededSkills.hacker.name);
+  t.equal('Senior Frontend', neededSkills.seniority.name);
 
-tap.test('Parse a seniority retrieved from the DB', function(t) {
-  t.equal('Jorge', parse.hacker.name);
-  t.equal('Senior Frontend', parse.level.name);
-  t.equal('Discuss Dependencies', parse.level.requirements[0].name);
+  tap.test('If the hacker don\'t have the skill', function(tt) {
+    var requirement = neededSkills.seniority.requirements[0]
+    tt.equal('Discuss Dependencies', requirement.name);
+    tt.equal(2, requirement.level.necessary);
+    tt.equal(-1, requirement.level.achieved);
+    tt.end();
+  })
+
+  tap.test('If the hacker have the skill', function(tt) {
+
+    tap.test('But don\'t have the necessary level', function(ttt) {
+      var requirement = neededSkills.seniority.requirements[1]
+      ttt.equal('TDD', requirement.name);
+      ttt.equal(2, requirement.level.necessary);
+      ttt.equal(1, requirement.level.achieved);
+      ttt.end();
+    })
+
+    tap.test('And achieved the necessary level', function(ttt) {
+      var requirement = neededSkills.seniority.requirements[2]
+      ttt.notEqual('JavaScript', requirement.name);
+      ttt.end();
+    })
+
+    tt.end();
+  })
+
   t.end();
 });
 
 tap.test('Get a needed skill', function(t) {
   tap.test('If the skill exists', function(tt) {
-    var skill = utils.getSkill(parse.level.requirements[0]);
-    tt.equal('Discuss Dependencies', skill.name);
-    tt.equal('Experience', skill.level);
+    var skill = utils.getSkill(seniority.requirements[1]);
+    tt.equal('TDD', skill.name);
+    tt.equal(2, skill.level.necessary);
+    tt.equal(-1, skill.description.indexOf('database'));
     tt.end();
   })
 
   tap.test('If the skill don\'t exists', function(tt) {
-    var skill = utils.getSkill(parse.level.requirements[1]);
+    var skill = utils.getSkill(seniority.requirements[2]);
     tt.equal('Time Traveler', skill.name);
-    tt.equal('Advanced', skill.level);
-    tt.equal('This skill isn\'t registered yet in our database.', skill.description);
-    tt.end();
-  })
-
-  t.end();
-});
-
-tap.test('Get the skill level for a needed skill', function(t) {
-  tap.test('If the hacker have the needed skill', function(tt) {
-    var level = utils.hackerSkillLevel('TDD');
-    tt.equal(1, level);
-    tt.end();
-  })
-
-  tap.test('If the hacker don\'t have the needed skill', function(tt) {
-    var level = utils.hackerSkillLevel('Discuss Dependencies');
-    tt.equal(-1, level);
+    tt.equal(3, skill.level.necessary);
+    tt.notEqual(-1, skill.description.indexOf('database'));
     tt.end();
   })
 
@@ -53,22 +77,19 @@ tap.test('Get the skill level for a needed skill', function(t) {
 
 tap.test('Build a level description for a needed skill', function(t) {
   tap.test('If the hacker have 1 point of difference', function(tt) {
-    skills[0].level = 'Experience'
-    var description = utils.buildLevelDescription(skills[0]);
+    var description = utils.buildLevelDescription(seniority.requirements[1]);
     tt.notEqual(-1, description.indexOf('Almost!'));
     tt.end();
   })
 
   tap.test('If the hacker have more than 1 point of difference', function(tt) {
-    skills[0].level = 'Advanced'
-    var description = utils.buildLevelDescription(skills[4]);
+    var description = utils.buildLevelDescription(seniority.requirements[3]);
     tt.notEqual(-1, description.indexOf('improve'));
     tt.end();
   })
 
   tap.test('If the hacker don\'t have the skill', function(tt) {
-    skills[1].level = 'Advanced'
-    var description = utils.buildLevelDescription(skills[1]);
+    var description = utils.buildLevelDescription(seniority.requirements[0]);
     tt.notEqual(-1, description.indexOf('knowledge'));
     tt.end();
   })
