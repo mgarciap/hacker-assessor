ab50c144840763a08ed069a0f0ebd9b4655deb7c
lcostantini <costantinileandro1@gmail.com>
2015-09-21 16:23:01 -0300

    Migrate from js stack to ruby stack on the backend

--- migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for migration in $(ls migrations)
-do
-  if [ "${migration##*.}" = "js" ]
-    then node "migrations/${migration}"
-  fi
-done
--- migrations/1442003208417-create-seniorities-table.js
+++
@@ -1,4 +0,0 @@
-var connection = require("../connection");
-var seniorities = require("../models/seniorities");
-
-connection().query(seniorities.create().toQuery().text, connection.logger);
