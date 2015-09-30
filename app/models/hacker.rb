62e6a760af511378585f849ff21de8bd71a67dfc
lcostantini <costantinileandro1@gmail.com>
2015-09-30 10:34:32 -0300

    Add has_meny relations to hacker and seniority.

--- app/models/hacker.rb
+++ app/models/hacker.rb
@@ -1,3 +1,5 @@
 class Hacker < ActiveRecord::Base
   has_secure_password
+  has_many :acquirements
+  has_many :skills, through: :acquirements
 end
--- app/models/seniority.rb
+++ app/models/seniority.rb
@@ -1,2 +1,4 @@
 class Seniority < ActiveRecord::Base
+  has_many :requirements
+  has_many :skills, through: :requirements
 end
