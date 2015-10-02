8cbf9d8e8d44c2f30024880701ebd70c0ec70660
lcostantini <costantinileandro1@gmail.com>
2015-10-02 11:43:25 -0300

    Add custom form to select skill.

--- app/form_builders/custom_form_builder.rb
+++ app/form_builders/custom_form_builder.rb
@@ -2,4 +2,8 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
   def skill_level_select attribute = :level
     collection_select attribute, SkillLevel.all, :id, :name
   end
+
+  def skill_select attribute = :skill_id
+    collection_select attribute, Skill.all, :id, :name, prompt: true
+  end
 end
