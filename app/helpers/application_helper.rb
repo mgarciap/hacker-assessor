e4f948d07bd9119701ec67eb7adee7cd0d3c6d44
rbournissent <rodrigobourni@hotmail.com>
2015-10-05 16:01:11 -0300

    Add *-accomplished classes and Use them on Show Seniority view (closes #39)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def md5 string
     Digest::MD5.hexdigest string
   end
+
+  def assign_class requirement, acquirement
+    if requirement.level > (acquirement.level + 1)
+      "not-accomplished"
+    elsif requirement.level <= acquirement.level
+      "accomplished"
+    else
+      "almost-accomplished"
+    end
+  end
 end
