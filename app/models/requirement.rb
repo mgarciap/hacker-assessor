b53a4013c83a1dac37b1acb05e378ac795b79f89
lcostantini <costantinileandro1@gmail.com>
2015-10-05 12:36:19 -0300

    Add model validations.

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,4 +1,7 @@
 class Requirement < ActiveRecord::Base
   belongs_to :seniority
   belongs_to :skill
+
+  validates :level, presence: true
+  validates :skill_id, presence: true
 end
--- app/models/seniority.rb
+++ app/models/seniority.rb
@@ -3,4 +3,5 @@ class Seniority < ActiveRecord::Base
   has_many :skills, through: :requirements
 
   accepts_nested_attributes_for :requirements, allow_destroy: true
+  validates :name, presence: true
 end
