1e28713d49b513f65e404bf49562e15663599c58
lcostantini <costantinileandro1@gmail.com>
2015-10-05 15:03:23 -0300

    Add more data to the seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,26 @@
 
 require 'csv'
 
+def acquirement_params
+  [ {"level"=>"1", "skill_id"=>"52"},
+    {"level"=>"2", "skill_id"=>"2"},
+    {"level"=>"2", "skill_id"=>"10"},
+    {"level"=>"3", "skill_id"=>"12"},
+    {"level"=>"3", "skill_id"=>"44"} ]
+end
+
+def requirements_params
+  [ {"skill_id"=>"52", "level"=>"3"},
+    {"skill_id"=>"2", "level"=>"2"},
+    {"skill_id"=>"10", "level"=>"3"},
+    {"skill_id"=>"12", "level"=>"3"},
+    {"skill_id"=>"44", "level"=>"3"},
+    {"skill_id"=>"51", "level"=>"3"},
+    {"skill_id"=>"3", "level"=>"2"},
+    {"skill_id"=>"13", "level"=>"1"},
+    {"skill_id"=>"8", "level"=>"0"} ]
+end
+
 %w[ applies_to_all_areas.csv devops.csv front_end.csv non_mesurable.csv
     ruby.csv ].each do |f|
   CSV.open Rails.root.join('db', 'seeds', f), headers: true do |t|
@@ -16,3 +36,9 @@ require 'csv'
     end
   end
 end
+
+hacker ||= Hacker.create email: 'jorge@altoros.com', password: 'jorge'
+acquirement_params.each { |acq| hacker.acquirements.create acq }
+
+seniority ||= Seniority.create name: 'Senior Frontend'
+seniority.requirements.create requirements_params
