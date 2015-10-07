11bf6bb0d3ef448050ca9fc6c9b4d1b5ed30c5d2
rbournissent <rodrigobourni@hotmail.com>
2015-10-07 11:29:06 -0300

    Add helper method for gravatar image.
    Add avatar class.
    Insert gravatar image into logout link.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,12 @@ module ApplicationHelper
       "almost-accomplished"
     end
   end
+
+  def gravatar_url hacker
+    'http://www.gravatar.com/avatar/' + md5(hacker.email)
+  end
+
+  def current_hacker_gravatar
+    image_tag(gravatar_url(current_hacker), class: 'avatar')
+  end
 end
