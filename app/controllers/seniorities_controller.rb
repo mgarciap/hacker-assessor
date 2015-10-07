56d0188cf6128a9b933cfd5d67f8d605e32f87d4
lcostantini <costantinileandro1@gmail.com>
2015-10-07 15:00:00 -0300

    Update controllers to use Responder.

--- app/controllers/seniorities_controller.rb
+++ app/controllers/seniorities_controller.rb
@@ -1,75 +1,48 @@
 class SenioritiesController < ApplicationController
   before_action :set_seniority, only: [:show, :edit, :update, :destroy]
 
-  # GET /seniorities
-  # GET /seniorities.json
+  respond_to :html
+
   def index
     @seniorities = Seniority.all
+    respond_with(@seniorities)
   end
 
-  # GET /seniorities/1
-  # GET /seniorities/1.json
   def show
+    respond_with(@seniority)
   end
 
-  # GET /seniorities/new
   def new
     @seniority = Seniority.new
     @seniority.requirements.build
+    respond_with(@seniority)
   end
 
-  # GET /seniorities/1/edit
   def edit
   end
 
-  # POST /seniorities
-  # POST /seniorities.json
   def create
     @seniority = Seniority.new(seniority_params)
-
-    respond_to do |format|
-      if @seniority.save
-        format.html { redirect_to @seniority, notice: 'Seniority was successfully created.' }
-        format.json { render :show, status: :created, location: @seniority }
-      else
-        format.html { render :new }
-        format.json { render json: @seniority.errors, status: :unprocessable_entity }
-      end
-    end
+    @seniority.save
+    respond_with(@seniority)
   end
 
-  # PATCH/PUT /seniorities/1
-  # PATCH/PUT /seniorities/1.json
   def update
-    respond_to do |format|
-      if @seniority.update(seniority_params)
-        format.html { redirect_to @seniority, notice: 'Seniority was successfully updated.' }
-        format.json { render :show, status: :ok, location: @seniority }
-      else
-        format.html { render :edit }
-        format.json { render json: @seniority.errors, status: :unprocessable_entity }
-      end
-    end
+    @seniority.update(seniority_params)
+    respond_with(@seniority)
   end
 
-  # DELETE /seniorities/1
-  # DELETE /seniorities/1.json
   def destroy
     @seniority.destroy
-    respond_to do |format|
-      format.html { redirect_to seniorities_url, notice: 'Seniority was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    respond_with(@seniority)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_seniority
       @seniority = Seniority.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def seniority_params
-      params.require(:seniority).permit(:name, { requirements_attributes: [:skill_id, :level] } )
+      params.require(:seniority).permit(:name, { requirements_attributes: [:skill_id, :level] })
     end
 end
