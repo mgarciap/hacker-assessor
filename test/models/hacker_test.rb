48f6684343b60d0db54b9b4a57775debf02dccf6
Eloy Espinaco <eloyesp@gmail.com>
2015-10-09 16:04:45 -0300

    Add missing test.

--- test/models/hacker_test.rb
+++ test/models/hacker_test.rb
@@ -4,4 +4,9 @@ class HackerTest < ActiveSupport::TestCase
   test "have a seniority" do
     assert_equal 2, hackers(:jorge).seniority
   end
+
+  test "knows the missing requirements to next level" do
+    missing = hackers(:jorge).missing_requirements_for_next_level
+    assert_includes missing, requirements(:senior_js_tdd)
+  end
 end
