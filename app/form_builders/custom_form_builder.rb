ab5e08c519fd1d226b0c5ce0f8b2dc0b4b9dac51
lcostantini <costantinileandro1@gmail.com>
2015-10-13 16:52:44 -0300

    Use custom select builder for seniority.

--- app/form_builders/custom_form_builder.rb
+++ app/form_builders/custom_form_builder.rb
@@ -7,4 +7,9 @@ class CustomFormBuilder < ActionView::Helpers::FormBuilder
   def skill_select attribute = :skill_id
     collection_select attribute, Skill.all, :id, :name, prompt: true
   end
+
+  def seniority_select attribute = :seniority
+    select attribute, Seniority.options,
+      selected: object.public_send(attribute).to_i
+  end
 end
